4668464681b50c903d9cb49fca389b2c5ed8fbc4
Alberto <alberto.vaccari@smarp.com>
2016-02-04 12:21:28 +0200

    updated watcher

--- watcher.go
+++ watcher.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"io"
-	"bufio"
-	"bytes"
-	"strings"
-	"path/filepath"
-	"github.com/andreaskoch/go-fswatch"
+    "os"
+    "fmt"
+    "io"
+    "time"
+    "bufio"
+    "bytes"
+    "strings"
+    "path/filepath"
+    "github.com/andreaskoch/go-fswatch"
 )
 
 var buildFolder = "build/"
@@ -22,8 +23,8 @@ const (
 // Start watcher
 func Start(templateFolder string) {
 
-	// Force building at startup
-	buildTemplates(os.Args[1])
+    // Force building at startup
+    buildTemplates(os.Args[1])
 
 
     recurse := true // include all sub directories
@@ -47,16 +48,18 @@ func Start(templateFolder string) {
 
         case changes := <-folderWatcher.ChangeDetails():
 
-        	switch {
-        		case len(changes.New()) > 0: {
-        			buildTemplates(templateFolder)
-				}
+            switch {
+                case len(changes.New()) > 0: {
+                    fmt.Println(time.Now().Format(time.Stamp) + ": " + changes.New()[0] + "...")
+                    buildTemplates(templateFolder)
+                }
 
-        		case len(changes.Modified()) > 0 : {
-		            buildTemplates(templateFolder)
-		        }
+                case len(changes.Modified()) > 0 : {
+                    fmt.Println(time.Now().Format(time.Stamp) + ": " + changes.Modified()[0] + "...")
+                    buildTemplates(templateFolder)
+                }
 
-        	} 
+            } 
         }
     }
 
@@ -65,20 +68,21 @@ func Start(templateFolder string) {
 // Execute main
 func executeFile(path string, folder string) string {
 
-	fmt.Println("Building " + path + "...")
- 	file, _ := os.Open(path)
+    file, _ := os.Open(path)
 
     var buffer bytes.Buffer
 
     scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-    	line := scanner.Text()
 
-    	if strings.HasPrefix(line, templateToken) {
-    		buffer.WriteString(executePartial(strings.Replace(line, templateToken, "", -1), folder) + "\n")
-    	} else {
-    		buffer.WriteString(line + "\n")
-    	}
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        if strings.HasPrefix(strings.TrimSpace(line), templateToken) {
+            buffer.WriteString(executePartial(strings.Replace(strings.TrimSpace(line), templateToken, "", -1), folder) + "\n")
+            
+        } else {
+            buffer.WriteString(line + "\n")
+        }
     }
 
   file.Close()
@@ -87,23 +91,26 @@ func executeFile(path string, folder string) string {
 
 // Execute Partial file
 func executePartial(path string, folder string) string {
- 	file, _ := os.Open(folder + "/" + path)
+    file, _ := os.Open(folder + "/" + path)
 
     var buffer bytes.Buffer
 
     scanner := bufio.NewScanner(file)
+
+    buffer.WriteString("\n<!-- " + path +" // -->\n")
     for scanner.Scan() {
-    	line := scanner.Text()
+        line := scanner.Text()
 
-    	if strings.HasPrefix(line, templateToken) {
-    		buffer.WriteString(executePartial(strings.Replace(line, templateToken, "", -1), folder) + "\n")
-    	} else {
-    		buffer.WriteString(line + "\n")
-    	}
+        if strings.HasPrefix(strings.TrimSpace(line), templateToken) {
+            buffer.WriteString(executePartial(strings.Replace(strings.TrimSpace(line), templateToken, "", -1), folder) + "\n")
+        } else {
+            buffer.WriteString(line + "\n")
+        }
     }
+    buffer.WriteString("<!-- // " + path +" -->\n")
 
-  file.Close()
-  return buffer.String()
+    file.Close()
+    return buffer.String()
 }
 
 func isFile(path string) bool {
@@ -122,10 +129,10 @@ func isFile(path string) bool {
         return false
     }
     switch mode := fi.Mode(); {
-	    case mode.IsDir():
-	        return false
-	    case mode.IsRegular():
-	        return true
+        case mode.IsDir():
+            return false
+        case mode.IsRegular():
+            return true
     }
 
     return false
@@ -133,47 +140,47 @@ func isFile(path string) bool {
 
 
 func buildTemplates(templateFolder string) {
+    // Go throuch each template
+    files, _ := filepath.Glob(templateFolder + "*")
+    
+    // Create output folder
+    os.Mkdir(buildFolder, 0777)
 
-	fmt.Println("Building...")
-	// Go throuch each template
-	files, _ := filepath.Glob(templateFolder + "*")
-	fmt.Println(files)
-	
-	// Create output folder
-	os.Mkdir(buildFolder, 0777)
+    for i := 0; i < len(files); i++ {
+        if isFile(files[i]) {
+            content := executeFile(files[i], templateFolder)
 
-	for i := 0; i < len(files); i++ {
-		if isFile(files[i]) {
-			content := executeFile(files[i], templateFolder)
+            f, err := os.Create(buildFolder + filepath.Base(files[i]))
 
-			f, err := os.Create(buildFolder + filepath.Base(files[i]))
+            if err != nil {
+                fmt.Println(err)
+            }
+            
+            n, err := io.WriteString(f, content)
 
-			if err != nil {
-			 	fmt.Println(err)
-			}
-	  		
-	  		n, err := io.WriteString(f, content)
+            if err != nil {
+                fmt.Println(n, err)
+            }
+            f.Close()
+        }
+    }
 
-		  	if err != nil {
-		   		fmt.Println(n, err)
-		  	}
-		  	f.Close()
-
-			fmt.Println("---------------------")
-		}
-	}
-
-	fmt.Println("Build complete!")
+    fmt.Println(time.Now().Format(time.Stamp) + ": Build complete!")
+    fmt.Println("===============================")
 
 }
 
 func main() {
 
-	if len(os.Args) > 2 {
-		buildFolder = os.Args[2]
-		fmt.Println("Folder changed to: " + buildFolder)
-	}
+    if len(os.Args) == 1 {
+        fmt.Println("Usage: go run template_builder.go <TEMPLATE_FOLDER> [BUILD_FOLDER]")
+        os.Exit(0)
+    }
+    if len(os.Args) > 2 {
+        buildFolder = os.Args[2]
+        fmt.Println("Folder changed to: " + buildFolder)
+    }
 
-	Start(os.Args[1])
+    Start(os.Args[1])
 
 }
